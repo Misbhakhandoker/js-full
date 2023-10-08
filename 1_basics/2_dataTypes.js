edee15f886be8a75cc560fac817e60c584a90fa3
MijbahMERN <misbhakhandoker@gmail.com>
2023-10-08 22:46:34 +0600

    dataTypes

--- 1_basics/2_dataTypes.js
+++ 1_basics/2_dataTypes.js
@@ -7,3 +7,23 @@ console.log(3
     3) // code readability should be high
 
 console.log("Mijbah")
+
+let name = "mijbah"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53  range
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
