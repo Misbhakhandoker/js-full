2a0355c4e9d57443a8a6b5afb0b2cfddfbb5a84c
MijbahMERN <misbhakhandoker@gmail.com>
2023-10-08 23:43:04 +0600

    conversionOperation

--- 1_basics/3_conversionOperation.js
+++ 1_basics/3_conversionOperation.js
@@ -1,11 +1,11 @@
 let score = null
 
-console.log(typeof score);
+// console.log(typeof score);
 
 
 let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 // "33" => 33
 // 33abc => NaN type=> number
@@ -25,5 +25,47 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 let someNumber = 33
 
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//  ***************************** Operations *****************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " Mijbah"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log((1 + 4) * 5 % 3);
+
+// console.log(true);
+// console.log(+true); // 1
+// console.log(true+); // error
+// console.log(+""); // 0
+
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter);
+
+// link to study
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
